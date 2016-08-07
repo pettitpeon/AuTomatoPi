1c0cd0db03668ba408c8eb4e7e9dd5e28cc8c787
Ivan Peon <pivo87@hotmail.com>
2016-08-07 20:54:19 +0200

    maintenace

--- BaseApi/src/BaLogMacros.h
+++ BaseApi/src/BaLogMacros.h
@@ -17,8 +17,13 @@
 #define BALOGMACROS_H_
 #include "BaseApi.h"
 
+/// @name Log Macros
+//@{
+/// Log with the given priority analog to @c printf() family. This assumes
+/// that TAG is defined. see @c BaApiLogF()
 #define TRACE_(fmt, ...) BaApiLogF(eBaLogPrio_Trace, TAG, fmt, ##__VA_ARGS__)
 #define WARN_(fmt, ...) BaApiLogF(eBaLogPrio_Warning, TAG, fmt, ##__VA_ARGS__)
 #define ERROR_(fmt, ...) BaApiLogF(eBaLogPrio_Error, TAG, fmt, ##__VA_ARGS__)
+//@}
 
 #endif // BALOGMACROS_H_
